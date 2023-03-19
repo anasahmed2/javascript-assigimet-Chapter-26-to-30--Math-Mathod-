1a14ac3c27218962d5f495ea4daab5faf63a4617
Anas Ahmed <123072865+anasahmed2@users.noreply.github.com>
2023-03-19 08:06:53 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+//*****************Chapter 26 to 30 ( Math Mathod)************************//
+
+//     Question 1
+
+// var num = + prompt("Enter the positive intgier")
+// var round =Math.round(num)
+// var floor =Math.floor(num)
+// var ceil =Math.ceil(num)
+
+// document.write("Number : " +num+"<br/>")
+// document.write("round off value  : " +round+"<br/>")
+// document.write("floor  : " +floor+"<br/>")
+// document.write("ceil value : " +ceil+"<br/>")
+
+
+//     Question 2
+
+// var num = + prompt("Enter the negative floating intgier")
+// var round =Math.round(num)
+// var floor =Math.floor(num)
+// var ceil =Math.ceil(num)
+
+// document.write("Number : " +num+"<br/>")
+// document.write("round off value  : " +round+"<br/>")
+// document.write("floor  : " +floor+"<br/>")
+// document.write("ceil value : " +ceil+"<br/>")
+
+
+//     Question 3
+
+// var num = +prompt("Enter a number")
+// var abss = Math.abs(num)
+// document.write("The absolute value of " + num + " is " + abss)
+
+
+//     Question 4
+
+// var num = Math.round((Math.random() * 5) + 1)
+// document.write("random dice value : " + num )
+
+
+//     Question 5
+
+// var answer;
+// var num = Math.round((Math.random() * 1) + 1)
+// if (num == 1) 
+//     answer = "head";
+// } else {
+//     answer = "tails"
+// }
+
+// document.write(num+"<br/> random coin value : " + answer)
+
+
+//     Question 6
+
+// var num = Math.ceil(Math.random() * 99)
+// document.write("The random value between 1 and 100 : " + num  )
+
+
+//     Question 7
+
+// ------------> Answer number 7 is skip due to wrong question  
+
+
+//     Question 8
+
+// var sceretNum  = 6;
+// var userNum = +prompt("Enter the finding number Between 1 to 10")
+
+// if (sceretNum=== userNum){
+//     alert("Congratulation you found the loss number "+sceretNum)
+// }else{
+//     alert("sorry please Try again")
+// }
+
+// console.log()
+
+//----------------------------End----------------------//
